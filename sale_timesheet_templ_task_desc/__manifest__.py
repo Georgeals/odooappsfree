2df817b35389fa5e1a8510c1ba38a3fc882a3147
Georgeals <george@artlinespb.ru>
2020-05-27 13:20:38 +0300

    Update __manifest__.py

--- sale_timesheet_templ_task_desc/__manifest__.py
+++ sale_timesheet_templ_task_desc/__manifest__.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
+# Copyright 2019 ArtLine Ltd <http://artlinespb.ru>, 2019
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
     'name': 'Sales Timesheet template task',
     'category': 'Hidden',
-    'summary': 'Modul add templeat task description',
+    'version': '11.0.1.0.1',  
+    'author': 'ArtLine',    
+    'summary': 'Module add templeat of task description',
     'description': """
-Allows create use template description of task
+Allows create and use template description of task
 =============================================
 
 This module auto add description for internal of product to the service task description.
 """,
     'website': 'http://artlinespb.ru/',
+    'images': ['static/description/banner.png'],    
     'depends': ['sale_timesheet'],
     'installable': True,
+    'license': 'AGPL-3',
     'uninstall_hook': 'uninstall_hook',
+    'support': 'george@artlinespb.ru',
+    "contributors": ["ArtLine <george@artlinespb.ru>"],
 }
