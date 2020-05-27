02f1745d85508492b33589445d41b0d291b02f00
Georgeals <george@artlinespb.ru>
2020-05-27 13:28:24 +0300

    edit

--- sale_timesheet_templ_task_desc/__init__.py
+++ sale_timesheet_templ_task_desc/__init__.py
@@ -3,15 +3,4 @@
 
 from odoo import api, SUPERUSER_ID
 
-from . import models
-from . import controllers
-
-
-def uninstall_hook(cr, registry):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['product.template'].search([
-        ('service_type', '=', 'timesheet')
-    ]).write({'service_type': 'manual'})
-    env['product.product'].search([
-        ('service_type', '=', 'timesheet')
-    ]).write({'service_type': 'manual'})
+from . import sale_order
