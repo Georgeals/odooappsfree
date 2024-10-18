2703541226e8797d2216618fa8f03484addfbce2
George <georgij.janguzov@midis.eu>
2024-10-18 14:15:55 +0300

    Fixed

--- account_reconcile_ext_artline/__manifest__.py
+++ account_reconcile_ext_artline/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "Block reconcile without partner",
-    "version": "11.0.0.1",
+    "version": "11.0.1.0.1",
     "summary": "Запрет сверки без указания партнера",
     "description": """
     """,
@@ -14,7 +14,5 @@
     "category": "Localization",  # todo: Категория неправильная. В задачах "добавления функций в других модулях" категорию можно подсмотреть в расширяемом модуле т.е. в account
     "sequence": 0,
     "depends": ["base", "account", "account_invoicing"],
-    "data": [
-    ],
     "installable": True,
 }
