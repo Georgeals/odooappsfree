7a0957a6f6feca02495b2fe4d8cf50588077a88c
Sergey Maximov <maksimov@artline-erp.ru>
2024-10-21 23:33:00 +0500

    fix: deleted code. Task: 3204

--- account_bank_statement_import_1c/wizards/account_bank_statement_import_1c.py
+++ account_bank_statement_import_1c/wizards/account_bank_statement_import_1c.py
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 # Copyright ArtLineStudio Ltd, 2019 (<https://artline-erp.ru>)
-# Author: George Yanguzov <george@artlinespb.ru>
+# Author: George Yanguzov <info@artline-erp.ru>
 # License AGPL-3.0 or later (<http://www.gnu.org/licenses/agpl>).
 from datetime import datetime
 
