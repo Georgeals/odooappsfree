da0220901cd90d0c29ef5c601a27fb7e6306f89f
Sergey Maximov <maksimov@artline-erp.ru>
2024-12-28 19:19:04 +0500

    refactor: due to todo's left in the code. task: 3096

--- l10n_ru_schet/models/res_bank.py
+++ l10n_ru_schet/models/res_bank.py
@@ -17,6 +17,3 @@ class Bank(models.Model):
 
 class ResPartnerBank(models.Model):
     _inherit = "res.partner.bank"
-
-    bank_corr_acc = fields.Char(related="bank_id.corr_acc", readonly=True)
-    # todo Зачем нужно это поле, если оно не выводится в карточке? readonly=True не нужно  By default, related fields are: readonly https://www.odoo.com/documentation/17.0/developer/reference/backend/orm.html#related-fields
